f20b768692ec60f6c2fd78b8a443f4757bb33608
Todd Holdman <todbot@gmail.com>
2014-04-27 13:01:57 -0500

    pg running locally

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Progress::Application.config.secret_key_base = '3bad9e34107b3d7e6ede316132ef4cce8bc40cd9a197d5a2f805cc9bd856163736b6e083c61ca73966c8fb1b73cf2d41925a5197e14e6fbe0293e0710a515416'
