7a0df8a889e9468ddbaefc9f015e8a46bcd85623
Todd Holdman <todbot@gmail.com>
2014-05-02 21:36:48 -0500

    adding ./ in front of required gem for f-ing heroku

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-  require 'ISBNdb'
+  require './ISBNdb'
 
   # GET /books
   # GET /books.json
